a70530b71ccdf1c5933480f32f4a86fc5d22b883
Nick Davis <ndavis55862@gmail.com>
2025-10-09 21:56:22 -0700

    change app.config names

--- studybuddy/app.config.js
+++ studybuddy/app.config.js
@@ -1,9 +1,9 @@
 import "dotenv/config"; //for .env variables
 export default {
     expo: {
-        name: "your-app-name",
-        slug: "your-app-slug",
-        scheme: "yourapp",
+        name: "StudyBuddy",
+        slug: "studybuddy",
+        scheme: "studdybuddy",
         extra: {
             SUPABASE_URL: process.env.SUPABASE_URL,
             SUPABASE_SECRET: process.env.SUPABASE_SECRET,
