074ea5c54a5ae69cf150b5f68ed511c4f1d47c8d
Nick Davis <ndavis55862@gmail.com>
2025-10-09 21:55:18 -0700

    add supabase and run reset script

---
+++ studybuddy/app/_layout.tsx
@@ -0,0 +1,7 @@
+import { Stack } from "expo-router";
+import "react-native-url-polyfill/auto";
+import "react-native-get-random-values";
+
+export default function RootLayout() {
+    return <Stack />;
+}
---
+++ studybuddy/lib/subapase.ts
@@ -0,0 +1,20 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+import Constants from "expo-constants";
+
+const { SUPABASE_URL, SUPABASE_SECRET } = Constants.expoConfig?.extra as {
+    SUPABASE_URL: string;
+    SUPABASE_SECRET: string;
+};
+console.log("PROCESS", process.env);
+console.log("Supabase URL:", SUPABASE_URL);
+console.log("Supabase ANON KEY:", SUPABASE_SECRET);
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_SECRET, {
+    auth: {
+        storage: AsyncStorage,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+});
