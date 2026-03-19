302d8faaab193bae6b14b2c281f00b775c26662f
Eli <elisanchezz767@gmail.com>
2026-03-18 23:01:58 -0700

    Added unit testing for bio

--- app/(app)/(tabs)/profile/index.tsx
+++ app/(app)/(tabs)/profile/index.tsx
@@ -110,12 +110,13 @@ export default function ProfileScreen() {
                             </TouchableOpacity>
                         </View>
                     </View>
-
-                    <View>
-                        <Text className="text-left color-colors-textSecondary">Bio</Text>
-                        <Text className="font-semibold text-2xl text-colors-text">{profile?.bio}</Text>
-                    </View>
                 </View>
+            </View>
+            <View className="w-full mt-0">
+                {/* <Text className="text-left color-colors-textSecondary mb-1">Bio</Text> */}
+                <Text className="font-semibold text-base text-colors-text">
+                    {profile?.bio?.trim() ? profile.bio : " "}
+                </Text>
             </View>
             <View className="w-full">
                 <Text className=" color-colors-textSecondary text-left mb-1">
