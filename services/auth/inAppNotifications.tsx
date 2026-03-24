375e6bd92cfbfaff63052231dcdf6ede220b6a3f
Johnae <Johnae4796@icloud.com>
2026-03-24 10:51:53 -0700

    Add push notification registration and update dependencies for in-app notifications

--- services/auth/inAppNotifications.tsx
+++ services/auth/inAppNotifications.tsx
@@ -1,5 +1,5 @@
-import supabase from "@/lib/subapase";
 import React, { createContext, useContext, useEffect, useState } from "react";
+import supabase from "../../lib/subapase";
 import { useAuth } from "./AuthProvider";
 
 type Notification = {
