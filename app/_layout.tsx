20ac20f54cae42d5517e1a8b624775f1a136ba6e
Johnae <Johnae4796@icloud.com>
2026-03-24 17:34:09 -0700

    Wrap StatusBar and notification providers in GestureHandlerRootView for improved gesture handling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import { ProfileProvider } from "@/services/ProfileProvider";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-get-random-values";
 import "./global.css";
 
@@ -12,15 +13,17 @@ export default function RootLayout() {
     return (
         <AuthProvider>
             <ProfileProvider>
-                <StatusBar style="light" />
-                <InAppNotificationProvider>
-                    <ActionSheetProvider>
-                        <>
-                            <Slot />
-                            <InAppNotificationBanner />
-                        </>
-                    </ActionSheetProvider>
-                </InAppNotificationProvider>
+                <GestureHandlerRootView>
+                    <StatusBar style="light" />
+                    <InAppNotificationProvider>
+                        <ActionSheetProvider>
+                            <>
+                                <Slot />
+                                <InAppNotificationBanner />
+                            </>
+                        </ActionSheetProvider>
+                    </InAppNotificationProvider>
+                </GestureHandlerRootView>
             </ProfileProvider>
         </AuthProvider>
     );
